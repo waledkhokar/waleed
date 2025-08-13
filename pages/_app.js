bff916c7bc9a18a720fb1e313f58a774c79ab80c
waledkhokar <waleedkhokhar780@gmail.com>
2025-08-13 15:02:19 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MIU LMS</title> {/* Change this to your desired app title */}
+        <meta name="description" content="MIU LMS" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
